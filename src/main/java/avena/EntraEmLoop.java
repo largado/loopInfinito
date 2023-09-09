ff96ab1883ffde831bf25ee76ceca82324e0b5f7
alexa <alexandrohipolito@gmail.com>
2023-09-09 13:11:31 -0300

    Initial commit

---
+++ src/main/java/avena/EntraEmLoop.java
@@ -0,0 +1,13 @@
+package avena;
+
+public class EntraEmLoop {
+    public static void main(String[] args) {
+
+        System.out.println("vou entrar em loop infinito!");
+
+        while (1 == 1){
+            System.out.println("estou no loop infinito......");
+        }
+
+    }
+}
